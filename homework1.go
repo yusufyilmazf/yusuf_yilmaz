d6f3401e3aeabda578b363d28a801e29d876bd01
Yusuf Yılmaz <109434872+yusufyilmazf@users.noreply.github.com>
2022-08-01 15:34:52 +0300

    Homework 1 Completed

--- homework1.go
+++ homework1.go
@@ -4,37 +4,44 @@ package main
 
 import "fmt"
 
+// Kat 1 Tanımlama
 func firstFloor(single int, totalroom int) int {
 
 	return single * totalroom
 }
 
+// Kat 2 Tanımlama
 func secondFloor(double int, totalroom int) int {
 
 	return double * totalroom
 }
 
+// Kat 1 Kapasitesini Tanımlama
 func firstfloorCap() int {
 	firstfloorcap := firstFloor(1, 18)
 	return firstfloorcap
 }
 
+// Kat 2 Kapasitesini Tanımlama
 func secondfloorCap() int {
 	secondfloorcap := secondFloor(2, 12)
 	return secondfloorcap
 }
 
+// Toplam Kapasiteyi Tanımlama
 func total() int {
 	total := firstfloorCap() + secondfloorCap()
 	return total
 }
 
+// Blok Sayısını Tanımlama
 func block() int {
 
 	block := 3
 	return block
 }
 
+//Hesaplama İşlemleri
 func main() {
 
 	if firstfloorCap() > secondfloorCap() {
